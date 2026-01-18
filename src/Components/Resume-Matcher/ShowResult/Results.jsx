d6b1151a856482560c2a2f0c316768be0d83cdf5
Ayan-Frontend <ayanalam3670@gmail.com>
2026-01-18 15:24:26 +0530

    styling improved

--- src/Components/Resume-Matcher/ShowResult/Results.jsx
+++ src/Components/Resume-Matcher/ShowResult/Results.jsx
@@ -8,12 +8,12 @@ const Results = () => {
       </div>
       <div className="mx-6 sm:mx-3 md:mx-6 lg:mx-10 mt-12 sm:mt-15 md:mt-20 mb-6 flex flex-col sm:flex-row sm:items-center justify-between lg:justify-evenly">
         <div className="capitalize">
-          <h3 className="text-2xl mb-6 sm:mb-0 sm:text-[28px] md:text-[33px] lg:text-4xl xl:text-4xl font-black">
+          <h3 className="text-2xl mb-6 sm:mb-0 sm:text-[28px] md:text-[33px] lg:text-4xl xl:text-3xl font-black">
             matched score :
           </h3>
         </div>
         <div className="xl:ml-20 px-8 sm:px-8 md:px-12 lg:px-15 py-6 bg-black text-white rounded-3xl border-4 border-blue-500">
-          <p className="capitalize text-center text-2xl md:text-[32px] lg:text-4xl font-bold">
+          <p className="capitalize text-center text-2xl md:text-[32px] lg:text-4xl xl:text-3xl font-bold">
             <span className="font-extrabold mr-3 lg:text-4xl xl:text-3xl text-blue-500 ">
               75%
             </span>{" "}
