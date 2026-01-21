dc0e78bedfa1e3df3bdc4ac3b365df05c0db0866
Ayan-Frontend <ayanalam3670@gmail.com>
2026-01-21 22:22:59 +0530

    new section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Home from "./Components/Home/Home";
 import Nav from "./Components/Navbar/Nav";
 import ResumeMatch from "./Components/Resume-Matcher/ResumeMatch";
+import Working from "./Components/HowItWork/Working";
 
 const App = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -11,6 +12,7 @@ const App = () => {
       <div className="sections-container mx-4 sm:mx-6 md:mx-8 lg:mx-10 xl:mx-12">
         <Home isMenuOpen={isMenuOpen} />
         <ResumeMatch />
+        <Working />
       </div>
     </>
   );
