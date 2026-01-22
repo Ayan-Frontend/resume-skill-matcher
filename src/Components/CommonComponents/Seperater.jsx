7cce822a76fb7b413ff3110ec04aea3284b29741
Ayan-Frontend <ayanalam3670@gmail.com>
2026-01-22 18:59:15 +0530

    styling improved

--- src/Components/CommonComponents/Seperater.jsx
+++ src/Components/CommonComponents/Seperater.jsx
@@ -1,6 +1,6 @@
 const Seperater = ({ width = 80 }) => {
   return (
-    <div className="mt-22 md:mt-24 xl:mt-18 mb-2 opacity-50 flex justify-center seperator">
+    <div className="mt-20 md:mt-24 xl:mt-18 mb-2 opacity-50 flex justify-center seperator">
       <div
         style={{ width: `${width}%` }}
         className="border-2 border-slate-300 rounded-2xl"
