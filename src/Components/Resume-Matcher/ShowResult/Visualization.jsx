01f791e7a023e283fce1111ba2b29cecbe6da72b
Ayan-Frontend <ayanalam3670@gmail.com>
2026-01-23 23:03:30 +0530

    the real time pie char displayed

--- src/Components/Resume-Matcher/ShowResult/Visualization.jsx
+++ src/Components/Resume-Matcher/ShowResult/Visualization.jsx
@@ -1,16 +1,61 @@
-const Visualization = () => {
+import { PieChart, Pie, Tooltip, ResponsiveContainer, Cell } from "recharts";
+
+const Visualization = ({ matchedSkills, missingSkills }) => {
+  const matchedCount = matchedSkills.length;
+  const missingCount = missingSkills.length;
+
+  const data = [
+    { name: "Matched", value: matchedCount },
+    { name: "Missing", value: missingCount },
+  ];
+
+  const COLORS = ["#3B82F6", "#EF4444"]; // blue, red
+
+  // Custom label function
+  const renderLabel = ({ name, percent }) => {
+    const p = Math.round(percent * 100);
+    return `${name} ${p}%`;
+  };
+
   return (
-    <div>
-      <div className="capitalize text-center">
-        <h2 className="mt-5 sm:mt-10 md:mt-8 lg:mt-10 mb-1 text-3xl md:text-4xl lg:text-5xl font-extrabold text-blue-500 ">
+    <div className="w-full">
+      <div className="capitalize text-center mb-5">
+        <h2 className="mt-6 sm:mt-10 md:mt-8 lg:mt-10 xl:mt-8 mb-1 text-3xl md:text-4xl lg:text-5xl font-extrabold text-blue-500 ">
           skills Visualization
         </h2>
         <p className="mb-3 font-semibold text-[18px] md:text-xl lg:text-2xl">
           demonstratation of scores
         </p>
       </div>
-      <div className="mt-10 mb-5 flex h-50 items-center justify-center rounded-xl border border-dashed text-gray-500">
-        Donut / Bar Chart Here
+      <div className="mt-6 w-full min-w-0 h-90">
+        <ResponsiveContainer width="100%" height="100%">
+          <PieChart>
+            <Pie
+              data={data}
+              dataKey="value"
+              nameKey="name"
+              outerRadius="80%"
+              label={renderLabel}
+              labelLine={false}
+            >
+              {data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index]} />
+              ))}
+            </Pie>
+            <Tooltip />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
+      {/* Small Legend */}
+      <div className="mt-4 flex justify-center gap-6 text-sm font-semibold">
+        <div className="flex items-center gap-2">
+          <span className="h-3 w-3 rounded-full bg-green-500"></span>
+          Matched: {matchedCount}
+        </div>
+        <div className="flex items-center gap-2">
+          <span className="h-3 w-3 rounded-full bg-red-500"></span>
+          Missing: {missingCount}
+        </div>
       </div>
     </div>
   );
